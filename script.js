b63f1f68728da1be7e8208577de0958dd7a5c2ae
Lipeburmann <lipeburmann720@gmail.com>
2026-07-16 21:57:47 -0300

    ajustado volume da musica

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const titulo = document.getElementById('titulo');
 const musicaFundo = document.getElementById('musicaFundo');
 const somEfeito = document.getElementById('somEfeito');
 const gifCumprimento = 'assets/mao_loop_count.gif';
-musicaFundo.volume = 0.08;
+musicaFundo.volume = 0.05;
 somEfeito.volume = 0.6;
 let timeoutComida = null;
 
